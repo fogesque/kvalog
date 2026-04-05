d2bb76de3d23b0be035bab27185ebc67e9a310bf
MatviiukPA <matviiukpa@spb.rdi-kvant.ru>
2026-04-05 13:53:38 +0300

    Update samples with coloring example

--- samples/kvalog_samples.cpp
+++ samples/kvalog_samples.cpp
@@ -7,7 +7,7 @@
 using namespace kvalog;
 
 // Example HTTP network adapter
-class HttpNetworkAdapter : public NetworkSinkInterface
+class HttpNetworkAdapter : public INetworkSink
 {
 public:
     explicit HttpNetworkAdapter(const std::string & endpoint) : endpoint(endpoint), connected(true)
@@ -16,7 +16,6 @@ public:
 
     void SendLog(const std::string & jsonLog) override
     {
-        // In real implementation: HTTP POST to endpoint
         std::cout << "[HTTP -> " << this->endpoint << "] " << jsonLog << std::endl;
     }
 
@@ -27,11 +26,11 @@ public:
 
 private:
     std::string endpoint;
-    bool connected;
+    bool connected = false;
 };
 
 // Example gRPC network adapter
-class GrpcNetworkAdapter : public NetworkSinkInterface
+class GrpcNetworkAdapter : public INetworkSink
 {
 public:
     explicit GrpcNetworkAdapter(const std::string & serverAddress)
@@ -41,7 +40,6 @@ public:
 
     void SendLog(const std::string & jsonLog) override
     {
-        // In real implementation: gRPC call
         std::cout << "[gRPC -> " << this->serverAddress << "] " << jsonLog << std::endl;
     }
 
@@ -52,94 +50,92 @@ public:
 
 private:
     std::string serverAddress;
-    bool connected;
+    bool connected = false;
 };
 
 void sampleBasicUsage()
 {
     std::cout << "\n=== Basic Usage Sample ===" << std::endl;
 
-    Logger::Config config;
+    auto config = Logger::Config();
     config.format = OutputFormat::Terminal;
     config.logToConsole = true;
 
-    Logger::Context ctx;
-    ctx.appName = "MyApplication";
-    ctx.moduleName = "MainModule";
+    auto context = Logger::Context();
+    context.appName = "MyApplication";
+    context.moduleName = "MainModule";
 
-    Logger logger(config, ctx);
+    auto logger = Logger::Create(config, context);
 
-    logger.Info("Application started successfully");
-    logger.Debug("Connected to database");
-    logger.Warning("Cache miss for key: user_123");
-    logger.Error("Failed to connect to remote service");
+    logger->Info("Application started successfully");
+    logger->Debug("Connected to database");
+    logger->Warning("Cache miss for key: user_123");
+    logger->Error("Failed to connect to remote service");
 }
 
 void sampleJsonLogging()
 {
     std::cout << "\n=== JSON Format Sample ===" << std::endl;
 
-    Logger::Config config;
+    auto config = Logger::Config();
     config.format = OutputFormat::Json;
     config.logToConsole = true;
 
-    Logger::Context ctx;
-    ctx.appName = "JsonApp";
-    ctx.moduleName = "MainModule";
+    auto context = Logger::Context();
+    context.appName = "JsonApp";
+    context.moduleName = "MainModule";
 
-    Logger logger(config, ctx);
+    auto logger = Logger::Create(config, context);
 
-    logger.Info("User logged in");
-    logger.Error("Transaction failed");
+    logger->Info("User logged in");
+    logger->Error("Transaction failed");
 }
 
 void sampleFieldConfiguration()
 {
     std::cout << "\n=== Field Configuration Sample ===" << std::endl;
 
-    Logger::Config config;
+    auto config = Logger::Config();
     config.format = OutputFormat::Terminal;
     config.logToConsole = true;
 
-    // Disable some fields
     config.fields.includeProcessId = false;
     config.fields.includeThreadId = false;
     config.fields.includeFile = false;
 
-    Logger::Context ctx;
-    ctx.appName = "MinimalApp";
-    ctx.moduleName = "MainModule";
+    auto context = Logger::Context();
+    context.appName = "MinimalApp";
+    context.moduleName = "MainModule";
 
-    Logger logger(config, ctx);
+    auto logger = Logger::Create(config, context);
 
-    logger.Info("Button clicked");
+    logger->Info("Button clicked");
 
-    // Re-enable fields at runtime
-    LogFieldConfig newFields;
+    auto newFields = LogFieldConfig();
     newFields.includeProcessId = true;
     newFields.includeThreadId = true;
-    logger.SetFieldConfig(newFields);
+    logger->SetFieldConfig(newFields);
 
-    logger.Info("Window resized");
+    logger->Info("Window resized");
 }
 
 void sampleFileLogging()
 {
     std::cout << "\n=== File Logging Sample ===" << std::endl;
 
-    Logger::Config config;
+    auto config = Logger::Config();
     config.format = OutputFormat::Json;
     config.logToConsole = true;
     config.logFilePath = "application.log";
 
-    Logger::Context ctx;
-    ctx.appName = "FileApp";
-    ctx.moduleName = "MainModule";
+    auto context = Logger::Context();
+    context.appName = "FileApp";
+    context.moduleName = "MainModule";
 
-    Logger logger(config, ctx);
+    auto logger = Logger::Create(config, context);
 
-    logger.Info("Logging to both console and file");
-    logger.Debug("Configuration loaded");
+    logger->Info("Logging to both console and file");
+    logger->Debug("Configuration loaded");
 
     std::cout << "Check 'application.log' file for JSON output" << std::endl;
 }
@@ -148,98 +144,94 @@ void sampleNetworkLogging()
 {
     std::cout << "\n=== Network Logging Sample ===" << std::endl;
 
-    // HTTP adapter
     auto httpAdapter = std::make_shared<HttpNetworkAdapter>("http://logstash.example.com:5000");
 
-    Logger::Config config;
+    auto config = Logger::Config();
     config.format = OutputFormat::Json;
-    config.logToConsole = false;  // Only network
+    config.logToConsole = false;
     config.networkAdapter = httpAdapter;
 
-    Logger::Context ctx;
-    ctx.appName = "NetworkApp";
-    ctx.moduleName = "MainModule";
+    auto context = Logger::Context();
+    context.appName = "NetworkApp";
+    context.moduleName = "MainModule";
 
-    Logger logger(config, ctx);
+    auto logger = Logger::Create(config, context);
 
-    logger.Info("Request received");
-    logger.Error("Invalid input data");
+    logger->Info("Request received");
+    logger->Error("Invalid input data");
 
-    // Switch to gRPC adapter
     std::cout << "\n--- Switching to gRPC ---" << std::endl;
     auto grpcAdapter = std::make_shared<GrpcNetworkAdapter>("localhost:50051");
 
-    Logger::Config grpcConfig;
+    auto grpcConfig = Logger::Config();
     grpcConfig.format = OutputFormat::Json;
     grpcConfig.networkAdapter = grpcAdapter;
 
-    Logger::Context grpcCtx;
-    grpcCtx.appName = "GrpcApp";
-    grpcCtx.moduleName = "MainModule";
+    auto grpcContext = Logger::Context();
+    grpcContext.appName = "GrpcApp";
+    grpcContext.moduleName = "MainModule";
 
-    Logger grpcLogger(grpcConfig, grpcCtx);
-    grpcLogger.Info("Streaming data");
+    auto grpcLogger = Logger::Create(grpcConfig, grpcContext);
+    grpcLogger->Info("Streaming data");
 }
 
 void sampleAsyncLogging()
 {
     std::cout << "\n=== Async Logging Sample ===" << std::endl;
 
-    Logger::Config config;
+    auto config = Logger::Config();
     config.format = OutputFormat::Terminal;
     config.logToConsole = true;
     config.asyncMode = Logger::Mode::Async;
-    config.asyncQueueSize = 8192;
+    config.asyncQueueSize = DefaultAsyncQueueSize;
     config.asyncThreadCount = 2;
 
-    Logger::Context ctx;
-    ctx.appName = "AsyncApp";
-    ctx.moduleName = "MainModule";
+    auto context = Logger::Context();
+    context.appName = "AsyncApp";
+    context.moduleName = "MainModule";
 
-    Logger logger(config, ctx);
+    auto logger = Logger::Create(config, context);
 
-    // Log from multiple threads
     auto worker = [&logger](int workerId) {
         for (int i = 0; i < 5; ++i) {
-            logger.Info("Worker" + std::to_string(workerId) + " is processing item" +
-                        std::to_string(i));
+            logger->Info("Worker" + std::to_string(workerId) + " is processing item" +
+                         std::to_string(i));
             std::this_thread::sleep_for(std::chrono::milliseconds(10));
         }
     };
 
-    std::thread t1(worker, 1);
-    std::thread t2(worker, 2);
-    std::thread t3(worker, 3);
+    auto thread1 = std::thread(worker, 1);
+    auto thread2 = std::thread(worker, 2);
+    auto thread3 = std::thread(worker, 3);
 
-    t1.join();
-    t2.join();
-    t3.join();
+    thread1.join();
+    thread2.join();
+    thread3.join();
 
-    logger.Flush();  // Ensure all async logs are written
+    logger->Flush();
 }
 
 void sampleCopyConfig()
 {
     std::cout << "\n=== Copy Config Sample ===" << std::endl;
 
-    Logger::Config config;
+    auto config = Logger::Config();
     config.format = OutputFormat::Terminal;
     config.fields.includeProcessId = false;
     config.logToConsole = true;
 
-    Logger::Context ctx;
-    ctx.appName = "MainService";
-    ctx.moduleName = "MainModule";
+    auto context = Logger::Context();
+    context.appName = "MainService";
+    context.moduleName = "MainModule";
 
-    Logger mainLogger(config, ctx);
-    mainLogger.Info("Main service started");
+    auto mainLogger = Logger::Create(config, context);
+    mainLogger->Info("Main service started");
 
-    Logger::Context newCtx;
-    newCtx.appName = "SubService";
-    newCtx.moduleName = "MainModule";
+    auto newContext = Logger::Context();
+    newContext.appName = "SubService";
+    newContext.moduleName = "MainModule";
 
-    // Create new logger with same config but different app name
-    Logger subLogger = Logger::WithConfigFrom(mainLogger, newCtx);
+    auto subLogger = Logger::WithConfigFrom(*mainLogger, newContext);
     subLogger.Info("Sub service started with inherited config");
 }
 
@@ -249,69 +241,87 @@ void sampleMultipleSinks()
 
     auto networkAdapter = std::make_shared<HttpNetworkAdapter>("http://logs.example.com");
 
-    Logger::Config config;
+    auto config = Logger::Config();
     config.format = OutputFormat::Json;
     config.logToConsole = true;
     config.logFilePath = "multi_sink.log";
     config.networkAdapter = networkAdapter;
 
-    Logger::Context ctx;
-    ctx.appName = "MultiSinkApp";
-    ctx.moduleName = "MainModule";
+    auto context = Logger::Context();
+    context.appName = "MultiSinkApp";
+    context.moduleName = "MainModule";
 
-    Logger logger(config, ctx);
+    auto logger = Logger::Create(config, context);
 
-    logger.Info("This goes to console, file, and network!");
-    logger.Critical("Critical Error logged everywhere");
+    logger->Info("This goes to console, file, and network!");
+    logger->Critical("Critical Error logged everywhere");
 }
 
 void sampleLogLevels()
 {
     std::cout << "\n=== Log Levels Sample ===" << std::endl;
 
-    Logger::Config config;
+    auto config = Logger::Config();
     config.format = OutputFormat::Terminal;
     config.logToConsole = true;
 
-    Logger::Context ctx;
-    ctx.appName = "LevelApp";
-    ctx.moduleName = "MainModule";
+    auto logger = Logger::Create(config);
 
-    Logger logger(config);
-
-    // Log at all levels
-    logger.Trace("Trace level message");
-    logger.Debug("Debug level message");
-    logger.Info("Info level message");
-    logger.Warning("Warning level message");
-    logger.Error("Error level message");
-    logger.Critical("Critical level message");
+    logger->Trace("Trace level message");
+    logger->Debug("Debug level message");
+    logger->Info("Info level message");
+    logger->Warning("Warning level message");
+    logger->Error("Error level message");
+    logger->Critical("Critical level message");
 
     std::cout << "\n--- Setting minimum level to Warning ---" << std::endl;
-    logger.SetLevel(LogLevel::Warning);
+    logger->SetLevel(LogLevel::Warning);
 
-    logger.Debug("This won't be shown");
-    logger.Info("This won't be shown either");
-    logger.Warning("This will be shown");
-    logger.Error("This will be shown too");
+    logger->Debug("This won't be shown");
+    logger->Info("This won't be shown either");
+    logger->Warning("This will be shown");
+    logger->Error("This will be shown too");
 
     std::cout << "\n--- Setting minimum level to Off ---" << std::endl;
-    logger.SetLevel(LogLevel::Off);
+    logger->SetLevel(LogLevel::Off);
 
-    logger.Warning("This won't be shown");
-    logger.Error("This won't be shown too");
+    logger->Warning("This won't be shown");
+    logger->Error("This won't be shown too");
 }
 
-void sampleFabricMethods() {
+void sampleColoredOutput()
+{
+    std::cout << "\n=== Colored Output Sample ===" << std::endl;
+
+    auto config = Logger::Config();
+    config.format = OutputFormat::Terminal;
+    config.logToConsole = true;
+    config.enableColors = true;
+
+    auto context = Logger::Context();
+    context.appName = "ColorApp";
+    context.moduleName = "MainModule";
+
+    auto logger = Logger::Create(config, context);
+
+    logger->Trace("Trace level message");
+    logger->Debug("Debug level message");
+    logger->Info("Info level message");
+    logger->Warning("Warning level message");
+    logger->Error("Error level message");
+    logger->Critical("Critical level message");
+}
+
+void sampleFabricMethods()
+{
     std::cout << "\n=== Fabric Methods Sample ===" << std::endl;
 
     auto logger = kvalog::CreateLogger("MyApp", "Main");
-        
-    logger.Info("Application started successfully");
-    logger.Debug("Connected to database");
-    logger.Warning("Cache miss for key: user_123");
-    logger.Error("Failed to connect to remote service");
 
+    logger->Info("Application started successfully");
+    logger->Debug("Connected to database");
+    logger->Warning("Cache miss for key: user_123");
+    logger->Error("Failed to connect to remote service");
 }
 
 int main()
@@ -326,7 +336,8 @@ int main()
     sampleAsyncLogging();
     sampleCopyConfig();
     sampleMultipleSinks();
-    sampleLogLevels(); 
+    sampleLogLevels();
+    sampleColoredOutput();
     sampleFabricMethods();
 
     std::cout << "\n=== All Samples Completed ===" << std::endl;
